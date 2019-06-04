32871933ab9452239d596fe77b6d35247791ca21
Danny Flynn <danfly@eircom.net>
2019-06-04 23:38:09 +0100

    Create swan.jl

---
+++ swan.jl
@@ -0,0 +1,29 @@
+input = readline();
+y = readline();
+
+#Let n be the input elements
+n = parse(Int64,input);
+
+#Let ar be the array of input integer
+ar = split(y);
+counter =0;
+
+#sort the array
+sort(ar)
+
+
+
+#count instances of number in an array
+
+function elementOccurrence(inputArray, num)
+	counter =0;
+	
+	for i in inputArray
+		inputArray[i]=0;
+	end
+	
+	return counter;
+	
+end
+
+elementOccurrence(ar, 2)
