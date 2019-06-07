81307e639160af2d41dcf8ac8ac4e12302aef721
Danny Flynn <danfly@eircom.net>
2019-06-07 22:28:35 +0100

    Summary

--- editable.jl
+++ editable.jl
@@ -1,44 +1,57 @@
-input = readline();
+x = readline();
+statedArraySize = parse(Int64, x)
 y = readline();
 
 #Let n be the input elements
-n = parse(Int64,input);
+n = parse(Int64,x);
 
 #Let ar be the array of input integer
 ar = split(y);
-occurrences=[]
-
-quanto=0;
 
 
-namelessArray= [1 2 3]
 
-#count instances of number in an array
-
-function elementOccurrence(inputArray::Array, numbah::Int64)
-	counter=0;
-	
-	for i in inputArray
-		i=parse(Int64,i)
-		if(i==numbah)
-			counter+=1
-		end
+#Function to get size of an array
+function sizeOf(input::Array)
+	counter =0;
+	for i in input
+	counter = counter+1;
 	end
-	
-	return counter;
-	
+	return counter
 end
 
-ar
+#Create variable for size of array
+arraySize = sizeOf(ar)
 
-#next step to check if elementOccurrence can be counted  through map
-#Redefine function interior
+#print predicted size of array
+println("The predicted size of the array was ",statedArraySize," The type of this variable is ",typeof(statedArraySize))
+#print out size of input array
+println("The size of the input array is ",arraySize, " The type of this variable is ", typeof(arraySize))
 
-for i in namelessArray
-		i=parse(Int64,i)
-		if(i==numbah)
-			counter+=1
-		end
-	end
-	
-#Such cannot be applied to elements
+@assert(statedArraySize==arraySize, "Size of input array does not correspond to given size")
+
+
+#Create an array of size n
+
+#Creat an integer array of given size
+function createArray(input)
+	result =Array{Union{Missing,Int64}}(missing,input)
+	return result
+end
+
+smoo =createArray(n)
+
+
+#convert every value in this array to 0
+for i in 1:n
+	smoo[i]=0
+end
+
+
+function cuber(input)
+	return input^3
+end
+
+for i in 1:n
+	smoo[i]=ar[i]
+end
+
