6bdc4a37400e6d5f48818d9ef427be33a64e75d0
Danny Flynn <danfly@eircom.net>
2019-06-11 21:12:30 +0100

    Update editable.jl

--- editable.jl
+++ editable.jl
@@ -1,1 +1,9 @@
-#Start again
+#Hiking
+
+#Gary is an avid hiker
+#He tracks his hikes meticulously, paying close attention to small details like topography
+#During his last hike he too exactly n steps. 
+#For ever step he took, he noted if it was an uphill U or downhill D step.
+#Gary's hikes start and end at sea level 
+#Each step up or down represents a 1 unit change in altitude
+
