836a7f88afa660a54a00ba3ffcb3f67b31ecb944
Danny Flynn <danfly@eircom.net>
2019-06-11 21:13:28 +0100

    Update editable.jl

--- editable.jl
+++ editable.jl
@@ -7,3 +7,6 @@
 #Gary's hikes start and end at sea level 
 #Each step up or down represents a 1 unit change in altitude
 
+#A mountain is a sequence of consecutive steps above sea level
+#starting with a step up from sea level
+#ending with a step down to sea level
