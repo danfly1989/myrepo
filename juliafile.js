4f527ba59c8503683596afa65ddfab429cee376a
Danny Flynn <danfly@eircom.net>
2019-06-13 20:41:08 +0100

    Levels

--- juliafile.js
+++ juliafile.js
@@ -1,2 +1,15 @@
 #Counter begins at sea level
-counter = 0;
+level = 0;
+
+isAtSeaLevel=false;
+isBelowSeaLevel=false;
+isAboveSeaLevel=false;
+
+if level==0
+	isAtSeaLevel=true;
+	
+if level<0
+	isBelowSeaLevel=true;
+
+if level>0
+	isAboveSeaLevel=true
