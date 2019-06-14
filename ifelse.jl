ad87f83c67b9079b9c4dc5fd6d774d3e4d56acd0
Danny Flynn <danfly@eircom.net>
2019-06-14 20:07:51 +0100

    Update ifelse.jl

--- ifelse.jl
+++ ifelse.jl
@@ -1,10 +1,10 @@
 if (1==1)
 	println("1==1 reached");
-	if(2==2)
-		println("2==2 reached");
+	if(2==0)
+		println("2==0 reached");
 	else
 		println("nested else reached");
-			if(9==9)
+			if(9==8)
 				println("nested if");
 			else
 				println("nested else")
