af6751e8e4a01cb908e79223ed19a1c7af683ed3
Danny Flynn <danfly@eircom.net>
2019-06-16 22:50:58 +0100

    Update editable.jl

--- editable.jl
+++ editable.jl
@@ -1,10 +1,3 @@
-#Gary is an avid hiker
-#He tracks his hikes meticulously, paying close attention to small details like topography
-#During his last hike he too exactly n steps. 
-#For ever step he took, he noted if it was an uphill U or downhill D step.
-#Gary's hikes start and end at sea level 
-#Each step up or down represents a 1 unit change in altitude
-
 #A mountain is a sequence of consecutive steps above sea level
 #starting with a step up from sea level
 #ending with a step down to sea level
