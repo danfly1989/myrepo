5109bb95eaae07e764b469083b5951abb42797e1
Danny Flynn <danfly@eircom.net>
2019-06-17 18:50:06 +0100

    Neat

--- alternate.jl
+++ alternate.jl
@@ -4,13 +4,13 @@ for i in 1:10
 	for j in 1:10
 	
 		if c<10
-			print("  ",c)
+			print("  ",c," ")
 			global c+=1
 		elseif c==100
 			print(c)
 	
 		else
-			print(" ",c)
+			print(" ",c," ")
 			global c+=1;
 		end
 	end
