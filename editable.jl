c0fb0ec3b9c63b92e466eb3072e5771d016402eb
Danny Flynn <danfly@eircom.net>
2019-06-21 09:49:48 +0100

    Update editable.jl

--- editable.jl
+++ editable.jl
@@ -1,4 +1,5 @@
-#Emma is playing a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads and others are cumulus. 
+#Emma is playing a new mobile game that starts with consecutively numbered clouds. 
+#Some of the clouds are thunderheads and others are cumulus. 
 #She can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus  or .
 #She must avoid the thunderheads. Determine the minimum number of jumps it will take Emma to jump from her starting postion to the last cloud. 
 #It is always possible to win the game.
