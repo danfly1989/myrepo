7f7c992dac87c13c5968de9cab60b8e44e148f14
Danny Flynn <danfly@eircom.net>
2019-06-22 19:34:47 +0100

    Update editable.jl

--- editable.jl
+++ editable.jl
@@ -1,12 +1,12 @@
 #Length of array
-input1 = readline()
+input1 = readline();
 x = parse(Int32,input1)
 
 #Cloud index array
-input2=readline()
+input2=readline();
 
 for i in input2
-	println(i)
+	println(i);
 end 
 
 #Create holder array
