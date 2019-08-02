ddd5dd9c38e7baf1ed8adcf153342b284627d959
Danny Flynn <danfly@eircom.net>
2019-08-02 11:39:53 +0200

    Update editable.jl

--- editable.jl
+++ editable.jl
@@ -10,34 +10,49 @@ x = parse(Int32,input1)
 #Cloud index array
 input2=readline();
 
+initialIndex=0;
+
+
+ counter = 1;
+ subCounter = 1;
+ 
+ array = [];
+
 for i in input2
-	#println(i);
-end 
-
-
-#problematic array
-arr= zeros(Int32, x,1);
-
-
-
-counter =1;
-for i in input2
-	#check line
-	#a1[counter]=input2[counter]; 
-	#global counter =counter+1;
+	if ((counter%2)!=0)
+		num = parse(Int32,i)
+		push!(array,num);
+	end
+	global counter = counter +1;
 end
+	
+array
 
-for i in input2
-	c = parse(Int32,i); 
-	#test
-	if(c==0)
-		println("thundercloud");
-	else
-		println("Not a thundercloud");
+#emma starts from 1
+ currentCloudIndex = 1;
+ jumpNum=0;
+
+for i in 1:x
+
+	if currentCloudIndex==x
+		println(jumpNum);
+		break;
 	end
 	
-	global arr[i]=c
+	if (i<=(x-2))&&array[currentCloudIndex+2]==0
+		global currentCloudIndex;
+		#("move two clouds");
+		currentCloudIndex=currentCloudIndex+2;
+		global jumpNum=jumpNum+1;
+	elseif((i<=(x-1))&&array[currentCloudIndex+1]==0)
+		#("move one cloud");
+		currentCloudIndex=currentCloudIndex+1;
+		jumpNum=jumpNum+1;
+	else	
+		println("unable to move");
+	end
+	
 	
 end
-
+	
 
