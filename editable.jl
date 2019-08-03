d42694aae50f232f02b2e3ffe1aff0df876818ca
Danny Flynn <danfly@eircom.net>
2019-08-03 22:08:59 +0200

    Update editable.jl

--- editable.jl
+++ editable.jl
@@ -1,3 +1,15 @@
+#Repeatable String
 
+subString= readline();
+N = parse(Int128,readline());
+long=length(subString);
+div = Int128(ceil(N/long));
+
+
+
+
+longString=(subString^div);
+
+println(longString);
 
 
