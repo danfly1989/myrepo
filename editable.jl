af902837cab8dd23b6e27206a5cf2edba3057390
Danny Flynn <danfly@eircom.net>
2019-08-03 21:37:05 +0200

    Update editable.jl

--- editable.jl
+++ editable.jl
@@ -1,28 +1,3 @@
-#Repeatable String
-
-#subString= readline();
-#N = parse(Int128,readline());
-#length = length(subString);
-
-#println("this "*7);
 
 
-#number of dollars
-x=0;
-#number of cents
-y =0;
-
-
-for i in 1:10000
-	global x=i;
-		for i in 1:10000
-			global y=i
-			if(
-			((101*x)+(101*y)+2)
-			==0
-			)
-				println("ha");
-			end
-		end
-end
 
