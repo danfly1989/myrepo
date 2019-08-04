103284534c40d2630ea6441b6a9a7c7e03c598a2
Danny Flynn <danfly@eircom.net>
2019-08-04 11:26:01 +0200

    Update editable.jl

--- editable.jl
+++ editable.jl
@@ -3,13 +3,37 @@
 subString= readline();
 N = parse(Int128,readline());
 long=length(subString);
-div = Int128(ceil(N/long));
+index = 0
+array =[]
+div = Int128(ceil(N/long))
+county = 0;
+toRemove = 0;
+index2 = 0;
+
+repeatString =subString^div
 
 
 
 
-longString=(subString^div);
-
-println(longString);
 
 
+for i in subString
+	global index = index+1;
+	if(i=='a')
+		push!(array,index)
+	end
+end
+
+
+for i in repeatString
+	global index2=index2+1;
+	if repeatString[index2]=='a'&&index2>N
+		global toRemove = toRemove+1;
+	end
+end
+
+
+AsInOneInstance =length(array);
+
+
+println((AsInOneInstance*div)-toRemove);
