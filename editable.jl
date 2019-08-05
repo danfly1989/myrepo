ce2000ec80b1fea5ffddb5e2865b0c9f3fc77080
Danny Flynn <danfly@eircom.net>
2019-08-05 06:50:56 +0200

    Update editable.jl

--- editable.jl
+++ editable.jl
@@ -11,30 +11,36 @@ county = 0;
 toRemove = 0;
 index2 = 0;
 
-repeatString =subString^div
+
+for i in subString
+	global index = index+1;
+	if(i=='a')
+		push!(array,index)
+	end
+end
 
 
-
-
-
-
-#for i in subString
-	#global index = index+1;
-	#if(i=='a')
-		#push!(array,index)
-	#end
-#end
-
-
-#for i in repeatString
-	#global index2=index2+1;
-	#if repeatString[index2]=='a'&&index2>N
-		#global toRemove = toRemove+1;
-	#end
-#end
 
 
 AsInOneInstance =length(array);
 
 
-println((AsInOneInstance*div)-toRemove);
+
+#calculate A's to remove
+
+#the difference between N and complete substring
+difference = N-(long*div);
+println(difference);
+
+if difference > 0
+	println("dnot all As are necessarily counted");
+
+elseif difference <0
+	println("trailing As must be removed");
+
+else
+
+end
+
+
+println(AsInOneInstance*div);
