511468d6e9701ec682776bea7596b4711fe7c8ea
Danny Flynn <danfly@eircom.net>
2019-08-11 10:27:30 +0200

    Create HasNextOne.jl

---
+++ HasNextOne.jl
@@ -0,0 +1,16 @@
+x = [1,0,1,1,0];
+
+
+for i in 1:4
+println(hasNext(x,i));
+end
+
+function hasNext(array,i)
+	result = false;
+	if(array[i+1]==1)
+		result = true;
+	else
+		result = false;
+	end
+	return result;
+end
