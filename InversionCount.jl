d9409865815b5ba96ac47e75f3782e18a99a7f75
Danny Flynn <danfly@eircom.net>
2019-08-19 19:57:57 +0200

    Update InversionCount.jl

--- InversionCount.jl
+++ InversionCount.jl
@@ -1,14 +1,15 @@
 #input=[1,2,5,3,7,8,6,4]
 input = [5 1 2 3 7 8 6 4]
 
-#	5 and 1	1
-#	5 and 2	2
-#	5 and 3	3
-#	7 and 6	4
-#	7 and 4	5
-#	8 and 6	6
-#	8 and 4	7
-#	6 and 4	8
+#	5 and 1		1
+#	5 and 2		2
+#	5 and 3		3
+#	5 and 4		4
+#	7 and 6		5		
+#	7 and 4		6
+#	8 and 6		7
+#	8 and 4		8
+#	6 and 4		9
 
 
 inversionCounter = 0;
